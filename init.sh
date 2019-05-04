b5daab854717bfcec1386ad2f8c00e01c0f12b88
Michael Halemba <michael.halemba@otto.de>
2019-05-04 20:17:23 +0200

    fix init script

--- init.sh
+++ init.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-sudo apt-get -y install docker docker-compose
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
+sudo apt-get update
+
+sudo apt-get -y install docker-compose
 
 sudo usermod -aG docker pi
 
